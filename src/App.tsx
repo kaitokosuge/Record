4ce08c88bcb74a78569885902cbe943d4666275a
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-21 16:08:32 +0900

    [add]tmdbの仕様確認完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [count, setCount] = useState(0);
+	const [data, setData] = useState<any>();
+	const getMovieData = async () => {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/335984?api_key=${
+				import.meta.env.VITE_TMBD_KEY
+			}&language=en`
+		);
+		const data = await res.json();
+		console.log("映画のデータだよ", data);
+		setData(data);
+	};
+	useEffect(() => {
+		getMovieData();
+	}, []);
+	return (
+		<>
+			<div style={{ display: "flex", width: "100%" }}>
+				<img
+					src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${data.poster_path}`}
+					style={{ width: "170px", display: "block" }}
+				/>
+				<p style={{ color: "white", fontSize: "70px", fontWeight: "bold" }}>
+					{data.title}
+				</p>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
